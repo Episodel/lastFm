1a53dfb82f541dcc09659e3719aa8332e103afaf
Yemshyn Vladimir <mmepisode@gmail.com>
2021-01-29 23:21:23 +0200

    first commit

---
+++ src/app/pages/home/genre-card-data.ts
@@ -0,0 +1,32 @@
+export const genreDate = [
+  {
+    color: 'tomato',
+    title: 'Rock',
+    image: '../../../../assets/genre-label/rock.png',
+  },
+  {
+    color: 'orange',
+    title: 'Hip-hop',
+    image: '../../../../assets/genre-label/hip-hop.png',
+  },
+  {
+    color: 'skyblue',
+    title: 'Pop',
+    image: '../../../../assets/genre-label/pop.png',
+  },
+  {
+    color: 'green',
+    title: 'Indie',
+    image: '../../../../assets/genre-label/indie.png',
+  },
+  {
+    color: 'black',
+    title: 'R&N',
+    image: '../../../../assets/genre-label/r&b.png',
+  },
+  {
+    color: 'purple',
+    title: 'Electro',
+    image: '../../../../assets/genre-label/electro.png',
+  },
+];
